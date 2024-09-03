46a50b77b53092aa48590dd5b3ffc1816d6f15a7
gudzsv <gudzsvmail@gmail.com>
2024-09-03 03:34:42 +0300

    added validation on duration field

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,16 @@ function App() {
 		});
 	}
 
+	const inputIsValid = userInput.duration >= 1;
+
 	return (
 		<>
 			<Header />
 			<UserInput onChange={handleChange} userInput={userInput} />
-			<Results input={userInput} />
+			{!inputIsValid && (
+				<p className='center'>Please enter a duration greater than zero</p>
+			)}
+			{inputIsValid && <Results input={userInput} />}
 		</>
 	);
 }
